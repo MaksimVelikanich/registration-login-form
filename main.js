1a71a78a72e00aca62aaac31ce4196acfe752ef7
1 <a1@1s-MacBook-Air.local>
2023-02-14 02:00:24 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById('check').onclick = function(){
+    let login = document.getElementById('log').value ;
+    let password = document.getElementById('pas').value ;
+    
+    checkLogin = Boolean(login)
+    checkPassword = Boolean(password)
+    if(checkLogin && checkPassword == true){
+        let info = [
+            { 'login': login },
+            { 'password': password }
+         ]
+         console.log(info)
+    }
+}
+
